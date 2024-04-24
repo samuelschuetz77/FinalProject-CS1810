8cc91db81b777feb33fb122294b2a2e56f0a2234
SamuelSchuetz77 <samuel.schuetz@students.snow.edu>
2024-04-24 17:00:57 -0600

    wednesday

---
+++ CSharp Backend/Program.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+
+string fileName = "UserCredentials.json";
+List<UsernamePassword> usersCredentials = new();
+if(File.Exists(fileName))
+{
+    var json = File.ReadAllText(fileName);
+    usersCredentials.AddRange(JsonSerializer.Deserialize<List<UsernamePassword>>(json));
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/usercredentials", () => usersCredentials);
+app.MapPost("/usercredentials", (UsernamePassword UandP) => 
+{
+    usersCredentials.Add(UandP);
+    var json = JsonSerializer.Serialize(usersCredentials);
+    File.WriteAllText(fileName, json); //will create
+});
+
+
+app.Run();
+
+public record UsernamePassword(string email, string password);
