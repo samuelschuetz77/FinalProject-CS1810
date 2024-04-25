14e8ccc34089902ab75e765eb8017a7794bf9aa2
SamuelSchuetz77 <samuel.schuetz@students.snow.edu>
2024-04-25 12:13:32 -0600

    swag

--- home-svc.js
+++ home-svc.js
@@ -2,7 +2,7 @@ export async function GetLoginCredentials()
 {
     var CredResult = null;
     
-    await fetch("http://localhost:5147/usercredentials")
+    await fetch("http://chess-tournament-hub.azurewebsites.net/usercredentials")
         .then(response => {
             if (response.status < 200 || response.status >= 300) {
                 throw new Error("Invalid request, server error code!");
